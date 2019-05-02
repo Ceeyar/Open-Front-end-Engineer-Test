fb5f264250da23a0935ed53b914c8cff49451986
CeeyarRbt <cndovela@retrorabbit.co.za>
2019-05-02 13:05:03 +0200

    corrected naming of the service

--- src/app/services/joke-service.ts
+++ src/app/services/joke-service.ts
@@ -2,13 +2,13 @@ import { Injectable } from "@angular/core";
 
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
-import { Joke } from "./../interfaces/joke";
+import { Joke } from "../interfaces/joke";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root',
 })
-export class CategoryService {
+export class JokeService {
 
     constructor(private http: HttpClient) {}
 
@@ -20,5 +20,10 @@ export class CategoryService {
     // HTTP request, get joke by Category:
     fetchJokeByCategory(cat: string):Observable<any> {
         return this.http.get<Joke>('https://api.chucknorris.io/jokes/random?category='+ cat);
+    }
+
+    // HTTP request, get a random joke:
+    fetchRandomJoke():Observable<any> {
+        return this.http.get<Joke>('https://api.chucknorris.io/jokes/random');
     }
 }
