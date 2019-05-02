4ef4e04841e88721c5804e0282b52e39768263c2
CeeyarRbt <cndovela@retrorabbit.co.za>
2019-05-02 13:05:40 +0200

    fixed styling on content component

--- src/app/views/content/content.component.ts
+++ src/app/views/content/content.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Joke } from './../../interfaces/joke';
+import { JokeService } from '../../services/joke-service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent implements OnInit {
+export class ContentComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  categories: string[]; 
+  joke$: Observable<Joke>;
+  jokeSubscription: Subscription;
+  jokeByCatSubscription: Subscription;
+
+  constructor(private JokeService: JokeService) { }
 
   ngOnInit() {
+
+      this.jokeSubscription = this.JokeService.fetchCategories().subscribe(res => {
+      this.categories = res
+
+    });
+
   }
 
+  tellJoke(cat) {
+    
+    this.jokeByCatSubscription = this.JokeService.fetchJokeByCategory(cat).subscribe(jokeObj => {
+      this.joke$ = jokeObj;
+    });
+  }
+
+  ngOnDestroy() {
+    this.jokeSubscription.unsubscribe();
+    this.jokeByCatSubscription.unsubscribe();
+  }
 }
